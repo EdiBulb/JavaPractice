a8f179987967c8f06eca3860987527d21b126a77
gnsqud24@naver.com <gnsqud24@naver.com>
2022-09-07 22:54:05 +0900

    Today I Learn

--- src/baekjoon/silver/s5_bj1181_1.java
+++ src/baekjoon/silver/s5_bj1181_1.java
@@ -1,6 +1,7 @@
 package baekjoon.silver;
 
 /*제목 : 단어 정렬*/
+/*블로그 참고*/
 
 /*문제 이해 :
 * 1. 단어를 n개 입력 받는다.
@@ -33,12 +34,19 @@ package baekjoon.silver;
 *
 * */
 
+/*블로그로 배운 것
+* 1. 배열을 특정한 규칙에 의해 정렬하고 싶은 경우, Arrays.sort에소드에 Comparator을 구현한다.
+* 2. Arrays.sort를 재정의하는 것
+* 3. Comparator 객체에 대한 것
+* */
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Comparator;
 
-public class s5_bj1181 {
+public class s5_bj1181_1 {
 
 
 
@@ -55,17 +63,28 @@ public class s5_bj1181 {
         for(int i=0;i<N;i++){
             arr[i] = bf.readLine();
         }
+
+        Arrays.sort(arr, new Comparator<String>() {
+            //compare 사용법 알기
+            @Override
+            public int compare(String o1, String o2) {
+                if(o1.length() == o2.length()){
+                    return o1.compareTo(o2); //길이가 같다면 사전으로 정렬한다.
+                }else {
+                    return o1.length() - o2.length(); //
+                }
+
+            }
+        });
+
+        //이전 값과 같은지 비교하기 위해서 i-1을 해야하므로 arr[0]을 먼저 출력한다.
+        System.out.println(arr[0]);
+        for(int i=1;i<N;i++){
+            if(!arr[i].equals(arr[i-1])){
+                System.out.println(arr[i]);
+            }
+        }
 //        System.out.println(arr.toString()); //toString()을 정확히 모르는구나.
-        System.out.println(Arrays.toString(arr));
+//        System.out.println(Arrays.toString(arr));
     }
 }
-
-
-
-
-
-
-
-
-
-
