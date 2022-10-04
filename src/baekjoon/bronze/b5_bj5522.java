2b710b6e2d14d92da8665d84c9908fd736ce1dba
gnsqud24@naver.com <gnsqud24@naver.com>
2022-10-04 23:29:02 +0900

    Today I Learn

---
+++ src/baekjoon/bronze/b5_bj5522.java
@@ -0,0 +1,15 @@
+package baekjoon.bronze;
+
+import java.util.Scanner;
+
+public class b5_bj5522 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int result = 0;
+
+        for(int i=0;i<5;i++){
+            result +=sc.nextInt();
+        }
+        System.out.println(result);
+    }
+}
