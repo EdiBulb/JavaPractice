d3f8f2ceed7778267503c0e30f64d0d2bbac8e8d
gnsqud24@naver.com <gnsqud24@naver.com>
2022-10-16 13:40:26 +0900

    Today I Learn

--- src/baekjoon/silver/s1_bj1931_1.java
+++ src/baekjoon/silver/s1_bj1931_1.java
@@ -4,16 +4,32 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
+/*제목 : 회의실 배정*/
 
+/*문제 이해 :
+* 1. 1개의 회의실과 N개의 회의가 있다. 그리고 회의실 사용표를 만든다.
+* 2. 회의 : 시작시간과 끝나는 시간이 있다. 끝나는 시간과 시작 시간은 겹칠 수 있음
+* 3. 최대한으로 가질 수 있는 회의의 개수를 구해라!!
+* */
+
+/*접근법
+* 1. 시작시간과 끝나는 시간을 갖는 회의(Meeting) 클래스를 만든다.
+* 2. 회의 종류 리스트를 만든다.
+* 3. 정렬 기준 메서드 정의- 종료시간이 가장 빠른 회의를 고른다. 이전 회의 종료 시간에 바로 올 수 있는 시작시간을 가진 회의를 분류하고, 그 중에서도 종료시간이 가장 빠른 회의를 고르는 것을 반복한다.
+* 4. 그리디 메소드 정의
+* 5. 정렬하기
+* 6. 그리디 메서드 실행하기*/
 public class s1_bj1931_1 {
-    static List<Time> list = new ArrayList<>();
-    static int N;
+    static List<Meeting> list = new ArrayList<>(); //회의 리스트
+    static int N;//회의 개수
 
-    static class Time{
+    /*1번*/
+    //Meeting 클래스 만들기 - 시작시간, 끝나는 시간 가짐
+    static class Meeting{
         int start;
         int end;
 
-        public Time(int start, int end){
+        public Meeting(int start, int end){
             this.start = start;
             this.end = end;
         }
@@ -23,45 +39,53 @@ public class s1_bj1931_1 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());//회의 개수
 
+        /*2번*/
+        //회의 종류 리스트 만들기
         for(int i=0;i<N;i++){
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
+            int start = Integer.parseInt(st.nextToken());//시작시간
+            int end = Integer.parseInt(st.nextToken());//끝나는 시간
 
-            list.add(new Time(start,end));
+            list.add(new Meeting(start,end));//회의 리스트에 회의를 추가
         }
 
+        /*5번*/
         compare_sort();
 
+        /*6번*/
         greedy();
     }
 
+    /*3번*/
+    //정렬 기준 메서드 정의
     static void compare_sort(){
-        Collections.sort(list, new Comparator<Time>() {
+        Collections.sort(list, new Comparator<Meeting>() {//회의 리스트를 매개변수로 받아서 재정렬하기
             @Override
-            public int compare(s1_bj1931_1.Time o1, s1_bj1931_1.Time o2) {
-                if(o1.end == o2.end){
-                    return o1.start - o2.start;
+            public int compare(s1_bj1931_1.Meeting o1, s1_bj1931_1.Meeting o2) {
+                if(o1.end == o2.end){//끝나는 시간이 같은 경우
+                    return o1.start - o2.start;//시작 시간을 기준으로 정렬
                 }
-                return o1.end - o2.start;
+                return o1.end - o2.end;//같지 않은 경우 끝나는 시간을 기준으로 정렬
             }
         });
     }
 
+    /*4번*/
+    //그리디 메서드 정의
     static void greedy(){
-        int count = 0;
-        int prev_end_time = 0;
+        int count = 0;//회의실 개수
+        int prev_end_time = 0; // 이전 회의 끝난 시간
 
         for(int i=0;i<N;i++){
-            Time time = list.get(i);
+            Meeting time = list.get(i);
 
-            if(prev_end_time <= time.start){
-                prev_end_time = time.end;
-                count++;
+            if(prev_end_time <= time.start){//현재 미팅의 시작시간과 이진 미팅의 끝나는 시간 비교해서 시작시간이 큰 경우만,
+                prev_end_time = time.end;//그 미팅의 끝나는 시간을 갱신해줌
+                count++;//회의 개수 +1
             }
         }
-        System.out.println(count);
+        System.out.println(count);//총 회의 개수 출력
     }
 }
--- src/baekjoon/silver/s4_bj11047.java
+++ src/baekjoon/silver/s4_bj11047.java
@@ -1,4 +1,69 @@
 package baekjoon.silver;
+/*제목 : 동전 0 */
+
+/*문제 이해 :
+* 1. K 금액을 만들기 위한 동전 최소 개수를 구해라*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+/*접근법 :
+* 1. 동전 종류 리스트를 만든다.
+* 2. 그리디 메소드를 만든다.
+* 3. 동전 종류 리스트를 내림차순 정렬한다.
+* 4. 메소드를 실행한다.
+* */
 
 public class s4_bj11047 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        StringTokenizer st = new StringTokenizer(s);
+        int N = Integer.parseInt(st.nextToken()); // 동전 종류
+        int K = Integer.parseInt(st.nextToken()); // 값 K
+
+        /*1번*/
+        ArrayList<Integer> coinList = new ArrayList<Integer>(); //동전 종류를 담는 리스트
+
+        //coinList에 동전 담기
+        for(int i=0;i<N;i++){
+            coinList.add(Integer.valueOf(br.readLine()));
+        }
+        
+        /*3번*/
+        //그리디 사용을 위해 가장 큰 값의 동전을 사용해야함으로, 내림차순 정렬하기
+        Collections.sort(coinList,Collections.reverseOrder());
+ 
+        //그리디 객체생성
+        GreedyAlgorithm gObject = new GreedyAlgorithm();
+        
+        /*4번*/
+        //coinFunc() 메소드로 총 코인 개수 출력하기
+        gObject.coinFunc(K, coinList);
+
+    }
+
+
+}
+
+/*2번*/
+//그리디 클래스
+class GreedyAlgorithm {
+    public void coinFunc(Integer price, ArrayList<Integer> coinList) {//가격과 동전 종류 리스트를 파라미터로 받으면 총 동전 개수를 출력하는 메소드
+        Integer totalCoinCount = 0; //K값을 최소로 만들기 위한 사용된 총 동전 개수
+        Integer coinNum = 0; //각 종류별 동전이 K를 최소한으로 만족하기 위해 필요한 동전의 개수
+
+        for (int index = 0; index < coinList.size(); index++) {//파라미터인 coinList의 길이만큼 반복한다.
+            coinNum = price / coinList.get(index);//각 동전별로 사용되어야 할 개수를 구함
+            totalCoinCount += coinNum;//사용되어야 할 coinNum 를 총 개수에 합한다.
+            price -= coinNum * coinList.get(index);//남은 가격을 구한다.
+        }
+        System.out.println(totalCoinCount);//총 사용된 동전 출력
+    }
 }
