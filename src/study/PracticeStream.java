2549c2f9a3ef172f5b5947c6fda3e81542d98ed2
gnsqud24@naver.com <gnsqud24@naver.com>
2022-11-03 12:54:17 +0900

    Today I Learn

--- src/study/PracticeStream.java
+++ src/study/PracticeStream.java
@@ -1,7 +1,24 @@
 package study;
 
-public class PracticeStream {
-    public static void main(String[] args) {
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class PracticeStream {
+    public static void main(String[] args) throws IOException {
+
+
+        try{
+            FileReader fin = new FileReader("c:\\test.txt");
+            int c;
+            while ((c=fin.read())!=-1){
+                System.out.print((char)c);
+            }
+            fin.close();
+        }catch (FileNotFoundException e){
+            System.out.println("파일을 열 수 없음");
+        } catch (IOException e){
+            System.out.println("입출력 오류");
+        }
     }
 }
