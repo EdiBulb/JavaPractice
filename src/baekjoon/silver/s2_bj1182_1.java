e74a9a148c061b716ced1cf029f62b6ad5da33bb
gnsqud24@naver.com <gnsqud24@naver.com>
2022-11-27 11:13:18 +0900

    Today I Learn

---
+++ src/baekjoon/silver/s2_bj1182_1.java
@@ -0,0 +1,44 @@
+package baekjoon.silver;
+
+import java.util.Scanner;
+
+/*제목 : 부분수열의 합*/
+/*접근법(다른 풀이 참고) :
+* 1. DFS 알고리즘을 적용해서 지금 위치의 원소를 선택하거나 선택하지 않거나 2가지 조건을 재귀함수로 계속 호출하였다.*/
+
+public class s2_bj1182_1 {
+
+    static int N, S, count = 0; // N: N개의 수, S: 더한값, count: 부분수열의 개수
+    static int[] num; //수열의 값을 넣는 배열
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt(); // N 입력받기
+        S = sc.nextInt(); // S 입력받기
+        num = new int[N]; // 수열 넣는 배열 만들기
+
+        //수열 값 넣기
+        for(int i=0;i<N;i++){
+            num[i] = sc.nextInt();
+        }
+
+        dfs(0,0);
+
+        if(S==0){ // count 합이 0인 경우, 공집합도 포함되므로 그 수를 하나 빼주고 출력한다.
+            count--;
+            System.out.println(count);
+        }else{
+            System.out.println(count);
+        }
+    }
+    private static void dfs(int depth, int sum){ // depth: 위치를 나타낸다. sum: 부분수열의 합
+        if(depth==N){ // 마지막 위치인 경우,
+            if(sum == S){ // 더한 값과 같을 경우,
+                count++; //부분수열 개수 +1
+            }
+            return;
+        }
+        dfs(depth+1, sum+num[depth]); // 지금 위치의 원소를 선택한다.
+        dfs(depth+1, sum); // 선택하지 않는다.
+    }
+}
--- src/fc_Algorithm/NQueen.java
+++ src/fc_Algorithm/NQueen.java
@@ -2,37 +2,41 @@ package fc_Algorithm;
 
 import java.util.ArrayList;
 
+//N*N의 크기에서 N개의 퀸이 존재할 수 있는 방법에 대한 클래스
 public class NQueen {
 
     //조건에 맞는지를 검사하는 메서드
-    public boolean isAvailable(ArrayList<Integer> candidate, Integer currentCol){ // 현재까지의 퀸 리스트,
+    public boolean isAvailable(ArrayList<Integer> candidate, Integer currentCol){ // 현재까지의 퀸 리스트, 해당 인덱스 번호
         Integer currentRow = candidate.size(); //
         for(int index = 0; index < currentRow; index++){ // 순회
-            if((candidate.get(index) == currentCol) || (Math.abs(candidate.get(index)-currentCol)==currentRow-index)){ // 해당 행에 대한 퀸의 위치와 동일한지 보기,
-                return false;
+            //promising 기법
+            if((candidate.get(index) == currentCol) || (Math.abs(candidate.get(index)-currentCol)==currentRow-index)){ // 수평 조건, 대각선 조건
+                return false; // 조건에 안 맞음
             }
         }
-        return true;
+        return true; // 해당 행에 퀸 위치 가능
     }
 
     //지금 체크해야하는 행, 퀸이있는 리스트
-    public void dfsFunc(Integer N, Integer currentRow, ArrayList<Integer> currentCandidate){
-        if(currentRow == N){ // 전체행을 다 돌았다. 그리고 적절한 위치를 찾았다.
-            System.out.println(currentCandidate); // 그때까지의 적절한 퀸의 위치를 담고 있다.
+    public void dfsFunc(Integer N, Integer currentRow, ArrayList<Integer> currentCandidate){// N:크기, currentRow: 지금 체크해야하는 행, 퀸이 있는 리스트
+        if(currentRow == N){ // currentRow == N 은 전체 행(N-1까지)을 돌았다는 의미이고 적절한 퀸의 위치를 찾았다는 의미이다.
+            System.out.println(currentCandidate); // 출력하기
             return ;
         }
-        for(int index = 0; index<N;index++){// 재귀호출할 때 처리하는 코드
-            if(this.isAvailable(currentCandidate, index)==true){ // 조건을 체크한다. 기존 퀸의 위치와 어느 위치를 체크할지, 그리고 그 위치가 적합하다.
+        //순회하기
+        for(int index = 0; index<N;index++){// 해당 열의 모든 위치를 체크하기
+            if(this.isAvailable(currentCandidate, index)==true){ // 기존의 퀸의 위치와 index의 위치의 조건을 확인한다. true라면 위치가 적합하다는 뜻이다.
                 currentCandidate.add(index);//넣는다.
-                this.dfsFunc(N, currentRow + 1, currentCandidate); //
-                currentCandidate.remove(currentCandidate.size()-1); //
+                //그 다음줄을 체크한다.
+                this.dfsFunc(N, currentRow + 1, currentCandidate);
+                //prunning 기법 - 가지치기
+                currentCandidate.remove(currentCandidate.size()-1); //이용가능하지 않은 퀸을 삭제하기
             }
         }
     }
 
     public static void main(String[] args) {
         NQueen nObject = new NQueen();
-        nObject.dfsFunc(4, 0, new ArrayList<>());
+        nObject.dfsFunc(4, 0, new ArrayList<>());//N: 4, 0부터 시작, 처음 candidate는 아무것도 없음
     }
-
 }
